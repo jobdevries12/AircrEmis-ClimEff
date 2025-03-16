a19012b6d3369d3a20d710ebffa22b1cd83cbbd9
ajohary <ajohary@student.tudelft.nl>
2025-03-16 10:05:13 +0100

    question 1 and 2

---
+++ A2_Q2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Question 1a
+calculating the ozone concentration based on the provided data.
+Plot zonal mean ozone mixing ratio and ozone concentrations
+'''
+
